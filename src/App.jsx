b2e9868e701f9e9ef5cbf8b0196db3f18ecfa76c
hamza42076 <164366638+hamza42076@users.noreply.github.com>
2025-09-05 09:37:05 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { Routes, Route, Navigate } from "react-router";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from './Pages/Home.jsx';
 import About from './Pages/About.jsx';
 import Contact from './Pages/Contact.jsx';
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
   const [email, setEmail] = useState("");
